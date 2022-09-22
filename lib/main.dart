602e7add21407c3c4a8fededd35803dec07e1832
Elkhan Julian Brillianshah <ubbeg2000@gmail.com>
2022-09-22 19:42:28 +0700

    change: nambahin ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
-import 'dart:io' show Platform;
+import 'dart:io' show Platform, sleep;
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
@@ -24,6 +25,11 @@ class _HomePageState extends State<HomePage> {
   bool _scanStarted = false;
   bool _connected = false;
 
+  int _rrsi_0 = 0;
+  int _rrsi_1 = 0;
+  int _rrsi_2 = 0;
+  int _rrsi_3 = 0;
+
   // Bluetooth related variables
   late DiscoveredDevice _ubiqueDevice;
   final flutterReactiveBle = FlutterReactiveBle();
@@ -42,17 +48,13 @@ class _HomePageState extends State<HomePage> {
       _scanStarted = true;
     });
 
-    print("ASDF");
-
     Permission permission = Permission.bluetooth;
     // PermissionStatus permission;
     // if (Platform.isAndroid) {
-    print("MI");
-    PermissionStatus permStatus = await permission.request();
+    await permission.request();
     await Permission.location.request();
-    print("DI");
-    print(permStatus);
-    permGranted = permStatus.isGranted;
+
+    // permGranted = permStatus.isGranted;
     // permission = await LocationPermissions().requestPermissions();
     //   if (permission == PermissionStatus.granted) permGranted = true;
     // } else if (Platform.isIOS) {
@@ -60,36 +62,53 @@ class _HomePageState extends State<HomePage> {
     // }
     // permGranted = true;
     // Main scanning logic happens here ⤵️
-    if (permGranted) {
-      print("ASDFHUHU");
-      _scanStream = flutterReactiveBle.scanForDevices(
-        withServices: [],
-        scanMode: ScanMode.opportunistic,
-      ).listen((device) {
-        // Change this string to what you defined in Zephyr
-        print("DUDU" + device.name);
-        if (device.name.contains("ESP")) {
+    // if (permGranted) {
+    _scanStream = flutterReactiveBle.scanForDevices(
+      withServices: [],
+      scanMode: ScanMode.balanced,
+    ).listen((device) {
+      if (device.name.contains("ESP")) {
+        print(device.name + " " + device.rssi.toString() + "dBm");
+        setState(() {
+          _ubiqueDevice = device;
+          _foundDeviceWaitingToConnect = true;
+        });
+        if (device.name.split("_")[1] == "0") {
           setState(() {
-            _ubiqueDevice = device;
-            _foundDeviceWaitingToConnect = true;
+            _rrsi_0 = device.rssi;
           });
         }
-      });
-    }
+        if (device.name.split("_")[1] == "1") {
+          setState(() {
+            _rrsi_1 = device.rssi;
+          });
+        }
+        if (device.name.split("_")[1] == "2") {
+          setState(() {
+            _rrsi_2 = device.rssi;
+          });
+        }
+        if (device.name.split("_")[1] == "3") {
+          setState(() {
+            _rrsi_3 = device.rssi;
+          });
+        }
+      }
+    });
+    // }
   }
 
-  void _connectToDevice() {
-    print("HASDF");
+  void _connectToDevice() async {
     // We're done scanning, we can cancel it
-    _scanStream.cancel();
-    print(_ubiqueDevice);
+    await _scanStream.cancel();
+
+    // sleep(Duration(seconds: 1));
     // Let's listen to our connection so we can make updates on a state change
-    Stream<ConnectionStateUpdate> _currentConnectionStream = flutterReactiveBle
-        .connectToAdvertisingDevice(
-            id: _ubiqueDevice.id,
-            prescanDuration: const Duration(seconds: 1),
-            withServices: [serviceUuid, characteristicUuid]);
-    _currentConnectionStream.listen((event) {
+    StreamSubscription<ConnectionStateUpdate> _currentConnectionStream;
+    _currentConnectionStream = flutterReactiveBle.connectToAdvertisingDevice(
+        id: _ubiqueDevice.id,
+        prescanDuration: const Duration(seconds: 1),
+        withServices: [serviceUuid, characteristicUuid]).listen((event) {
       print(event);
       switch (event.connectionState) {
         // We're connected and good to go!
@@ -103,6 +122,15 @@ class _HomePageState extends State<HomePage> {
               _foundDeviceWaitingToConnect = false;
               _connected = true;
             });
+            print(_ubiqueDevice.rssi);
+            try {
+              flutterReactiveBle.writeCharacteristicWithoutResponse(
+                _rxCharacteristic,
+                value: [_ubiqueDevice.rssi],
+              );
+            } catch (e) {
+              print(e);
+            }
             break;
           }
         // Can add various state state updates on disconnect
@@ -113,15 +141,73 @@ class _HomePageState extends State<HomePage> {
         default:
       }
     });
+
+    // flutterReactiveBle.clearGattCache(_ubiqueDevice.id);
+    // _connected = false;
+
+    _currentConnectionStream.cancel();
+
+    _startScan();
+  }
+
+  // def find_angle(panjang1, panjang2, panjang3):
+  //   a = math.acos((-pow(panjang1, 2) + pow(panjang2, 2) +
+  //                 pow(panjang3, 2)) / (2*panjang2*panjang3))
+  //   b = math.acos((-pow(panjang2, 2) + pow(panjang3, 2) +
+  //                 pow(panjang1, 2)) / (2*panjang3*panjang1))
+  //   c = math.acos((-pow(panjang3, 2) + pow(panjang1, 2) +
+  //                 pow(panjang2, 2)) / (2*panjang1*panjang2))
+  //   return a, b, c
+
+  List<double> _findAngle(num l1, num l2, num l3) {
+    double a = acos((-pow(l1, 2) + pow(l2, 2) + pow(l3, 2)) / (2 * l2 * l3));
+    double b = acos((-pow(l2, 2) + pow(l3, 2) + pow(l1, 2)) / (2 * l3 * l1));
+    double c = acos((-pow(l3, 2) + pow(l1, 2) + pow(l2, 2)) / (2 * l1 * l2));
+    return [a, b, c];
+  }
+
+  List<double> _findCoords(num l1, num l2, num l3) {
+    // def find_koordinat(l1, l2, l3):
+    var a = _findAngle(l1, l2, l3);
+    var temp1 = cos(a[1]) * l3;
+    var temp2 = sin(a[1]) * l3;
+    return [temp1, temp2, temp1, -temp2];
   }
 
   void _partyTime() {
     print(_connected);
+    print(_rxCharacteristic);
+
+    // int rssi = _ubiqueDevice.rssi;
+
+    // flutterReactiveBle.statusStream.listen((status) {
+    //   BleStatus.
+    // });
+
+    // _scanStream = flutterReactiveBle.scanForDevices(
+    //   withServices: [],
+    //   scanMode: ScanMode.balanced,
+    // ).listen((device) {
+    //   if (device.name.contains("ESP")) {
+    //     setState(() {
+    //       _ubiqueDevice = device;
+    //       _foundDeviceWaitingToConnect = true;
+    //     });
+    //     rssi = device.rssi;
+    //     _scanStream.cancel();
+    //   }
+    // });
+
     if (_connected) {
-      flutterReactiveBle
-          .writeCharacteristicWithoutResponse(_rxCharacteristic, value: [
-        0xff,
-      ]);
+      // try {
+      print(_ubiqueDevice.rssi);
+      flutterReactiveBle.writeCharacteristicWithoutResponse(_rxCharacteristic,
+          value: [
+            _ubiqueDevice.rssi
+          ]).onError((error, stackTrace) => print(error));
+      // } catch (e) {
+      //   print(e);
+      // }
     }
   }
 
@@ -129,30 +215,65 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Container(),
+      body: Container(
+        padding: EdgeInsets.fromLTRB(0, 300, 0, 0),
+        child: Center(
+            child: Column(
+          children: [
+            Text(
+              (pow(10, (-72 - _rrsi_0) / (10 * 2))).toString() +
+                  "m, $_rrsi_0 dBm",
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              (pow(10, (-72 - _rrsi_1) / (10 * 2))).toString() +
+                  "m, $_rrsi_1 dBm",
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              (pow(10, (-72 - _rrsi_2) / (10 * 2))).toString() +
+                  "m, $_rrsi_2 dBm",
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              (pow(10, (-72 - _rrsi_3) / (10 * 2))).toString() +
+                  "m, $_rrsi_3 dBm",
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              _findCoords(
+                      pow(10, (-72 - _rrsi_0) / (10 * 2)),
+                      pow(10, (-72 - _rrsi_1) / (10 * 2)),
+                      pow(10, (-72 - _rrsi_2) / (10 * 2)))
+                  .toString(),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        )),
+      ),
       persistentFooterButtons: [
         // We want to enable this button if the scan has NOT started
         // If the scan HAS started, it should be disabled.
         // _scanStarted
-        //     // True condition
-        //     ? ElevatedButton(
-        //         // style: ElevatedButton.styleFrom(
-        //         //   primary: Colors.grey, // background
-        //         //   onPrimary: Colors.white, // foreground
-        //         // ),
-        //         onPressed: () {},
-        //         child: const Icon(Icons.search),
-        //       )
-        //     // False condition
-        //     :
+        // True condition
+        // ?
         ElevatedButton(
-          // style: ElevatedButton.styleFrom(
-          //   primary: Colors.blue, // background
-          //   onPrimary: Colors.white, // foreground
-          // ),
+          style: ElevatedButton.styleFrom(
+            primary: Colors.blue, // background
+            onPrimary: Colors.white, // foreground
+          ),
           onPressed: _startScan,
           child: const Icon(Icons.search),
         ),
+        // False condition
+        // : ElevatedButton(
+        //     style: ElevatedButton.styleFrom(
+        //       primary: Colors.blue, // background
+        //       onPrimary: Colors.white, // foreground
+        //     ),
+        //     onPressed: _startScan,
+        //     child: const Icon(Icons.search),
+        //   ),
         // _foundDeviceWaitingToConnect
         //     // True condition
         //     ?
@@ -175,13 +296,14 @@ class _HomePageState extends State<HomePage> {
         //   ),
         // _connected
         //     // True condition
-        //     ?
         ElevatedButton(
           style: ElevatedButton.styleFrom(
             primary: Colors.blue, // background
             onPrimary: Colors.white, // foreground
           ),
-          onPressed: _partyTime,
+          onPressed: () {
+            _scanStream.cancel();
+          },
           child: const Icon(Icons.celebration_rounded),
         ),
         // False condition
